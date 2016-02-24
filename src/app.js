204cf9b4e82ecae44aa9f5bcd3b5e327efc7522c
Eric Lifka <eric.lifka@gmail.com>
2016-02-23 19:04:24 -0500

    express hello world

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('app');
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => res.send("Hello World!"));
+app.listen(3000, () => console.log('listening on 3000'));
+
+export default app;
