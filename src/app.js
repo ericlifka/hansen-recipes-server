0949e7d54be3bbd0f934c5992373ed87f5011e1f
Eric Lifka <eric.lifka@gmail.com>
2016-02-26 22:45:34 -0500

    tieing routes into sequelize

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ var User = sequelize.define('user', {
 User.sync({force: true}).then(function () {
   // Table created
   return User.create({
+    id: 1,
     username: 'testabc',
     password: '123'
   });
@@ -41,7 +42,14 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(function (id, done) {
-  done(null, { username: "deserialized-user", id: id });
+  User.findOne({
+    where: { id }
+  }).then(function (user) {
+    done(null, user);
+  }).catch(function (err) {
+    done(err, null);
+  });
+  //done(null, { username: "deserialized-user", id: id });
 });
 
 function isAuthenticated(req, res, next) {
@@ -53,19 +61,22 @@ function isAuthenticated(req, res, next) {
 
 passport.use(new LocalStrategy(function (username, password, done) {
   console.log(`auth request: ${username}, ${password}`);
+  User.findOne({
+    where: { username }
+  });
   return done(null, { username: "test-user", id: "1" });
 }));
 
 app.get('/', (req, res) => res.send("Hello World!"));
 
 app.post('/login', passport.authenticate('local'), function (req, res) {
-  res.json({msg: "success"});
+  res.json({ msg: "success", user: req.user });
 });
 
 app.get('/api/users/me',
   isAuthenticated,
   function(req, res) {
-    res.json({ id: req.user.id, username: req.user.username });
+    res.json({ id: req.user.id, username: req.user.username, user: req.user });
   });
 
 app.post('/logout', function(req, res){
