4782f809001b8f99ddbaf62380242b7b32767f1b
Eric Lifka <eric.lifka@gmail.com>
2016-02-27 17:33:06 -0500

    get fancy with es6 functional style, probably more than is advisable

--- api/helpers/password-validation.js
+++ api/helpers/password-validation.js
@@ -1,19 +1,9 @@
 import bcrypt from 'bcrypt';
 
-export function hashPassword(password) {
-  return new Promise(function (resolve, reject) {
-    bcrypt.genSalt(10, function (saltErr, salt) {
-      if (saltErr) {
-        return reject(saltErr);
-      }
-
-      bcrypt.hash(password, salt, function (hashError, hash) {
-        if (hashError) {
-          return reject(hashError);
-        }
-
-        resolve(hash);
-      });
-    });
-  });
-}
+export const hashPassword = password =>
+  new Promise((resolve, reject) =>
+    bcrypt.genSalt(10, (saltError, salt) =>
+      saltError ? reject(saltError) :
+        bcrypt.hash(password, salt, (hashError, hash) =>
+          hashError ? reject(hashError) :
+            resolve(hash))));
