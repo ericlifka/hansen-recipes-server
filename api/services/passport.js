abb1e17d0711ed1421b6780c4564185a329d51fc
Eric Lifka <eric.lifka@gmail.com>
2016-02-27 14:17:21 -0500

    some cleanup for passport service, not touching the main body yet

--- api/services/passport.js
+++ api/services/passport.js
@@ -1,15 +1,7 @@
-var passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  bcrypt = require('bcrypt');
-
-//helper functions
-function findById(id, fn) {
-  User.findOne(id).then(function (user) {
-    return fn(null, user);
-  }).catch(function (err) {
-    return fn(null, null);
-  });
-}
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import passportLocal from 'passport-local';
+const LocalStrategy = passportLocal.Strategy;
 
 function findByUsername(u, fn) {
   User.findOne({
@@ -21,56 +13,38 @@ function findByUsername(u, fn) {
   });
 }
 
-// Passport session setup.
-// To support persistent login sessions, Passport needs to be able to
-// serialize users into and deserialize users out of the session. Typically,
-// this will be as simple as storing the user ID when serializing, and finding
-// the user by ID when deserializing.
-passport.serializeUser(function (user, done) {
-  done(null, user.id);
-});
+passport.serializeUser((user, done) =>
+  done(null, user.id));
 
-passport.deserializeUser(function (id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
+passport.deserializeUser((id, done) =>
+  User.findOne(id)
+    .then(user => done(null, user))
+    .catch(err => done(null, null)));
 
-// Use the LocalStrategy within Passport.
-// Strategies in passport require a `verify` function, which accept
-// credentials (in this case, a username and password), and invoke a callback
-// with a user object.
 passport.use(new LocalStrategy(
   function (username, password, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      // Find the user by username. If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message. Otherwise, return the
-      // authenticated `user`.
-      findByUsername(username, function (err, user) {
-        if (err)
-          return done(null, err);
-        if (!user) {
-          return done(null, false, {
-            message: 'Unknown user ' + username
-          });
-        }
-        bcrypt.compare(password, user.password, function (err, res) {
-          if (!res)
-            return done(null, false, {
-              message: 'Invalid Password'
-            });
-          var returnUser = {
-            username: user.username,
-            createdAt: user.createdAt,
-            id: user.id
-          };
-          return done(null, returnUser, {
-            message: 'Logged In Successfully'
-          });
+    findByUsername(username, function (err, user) {
+      if (err)
+        return done(null, err);
+      if (!user) {
+        return done(null, false, {
+          message: 'Unknown user ' + username
         });
-      })
+      }
+      bcrypt.compare(password, user.password, function (err, res) {
+        if (!res)
+          return done(null, false, {
+            message: 'Invalid Password'
+          });
+        var returnUser = {
+          username: user.username,
+          createdAt: user.createdAt,
+          id: user.id
+        };
+        return done(null, returnUser, {
+          message: 'Logged In Successfully'
+        });
+      });
     });
   }
 ));
