d797b381f25bf297da65457eb1b5500d2c0135a9
Eric Lifka <eric.lifka@gmail.com>
2016-02-27 19:51:28 -0500

    these blueprints aren't useful, I don't know why the tutorial created them

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -39,21 +39,3 @@ module.exports = {
     });
   }
 };
-
-module.exports.blueprints = {
-
-  // Expose a route for every method,
-  // e.g.
-  // `/auth/foo` =&gt; `foo: function (req, res) {}`
-  actions: true,
-
-  // Expose a RESTful API, e.g.
-  // `post /auth` =&gt; `create: function (req, res) {}`
-  rest: true,
-
-  // Expose simple CRUD shortcuts, e.g.
-  // `/auth/create` =&gt; `create: function (req, res) {}`
-  // (useful for prototyping)
-  shortcuts: true
-
-};
