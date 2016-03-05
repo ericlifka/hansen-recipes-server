ca5ebf9cc64f9d72a2cca67c374692c775a54909
Eric Lifka <eric.lifka@gmail.com>
2016-03-04 22:41:45 -0500

    a measurement record to hold things like '.25 cups of beef stock'

---
+++ api/models/Measurement.js
@@ -0,0 +1,29 @@
+/**
+ * Measurement.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    quantity: {
+      type: 'number',
+      required: true
+    },
+
+    unit: {
+      type: 'string'
+    },
+
+    ingredient: {
+      model: 'ingredient'
+    },
+
+    recipe: {
+      model: 'recipe'
+    }
+  }
+};
+
