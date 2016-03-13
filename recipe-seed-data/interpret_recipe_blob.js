69e88618ccc8af735d4aaee3d0cb8c19d4293990
Eric Lifka <eric.lifka@gmail.com>
2016-03-13 16:58:53 -0400

    modify to work on an arbitrary number of files

--- recipe-seed-data/interpret_recipe_blob.js
+++ recipe-seed-data/interpret_recipe_blob.js
@@ -1,42 +1,50 @@
 "use strict";
 const fs = require('fs');
 
-let textblob = fs.readFileSync('./appetizers.txt', "utf8");
-
-let recipes = textblob.split('\n\n');       // blank lines denote recipe blobs
-recipes = recipes.map(r => r.split('\n'));  // turn each blob into an array of lines
-recipes = recipes.map(recipeLines => {
-  let name = recipeLines.shift();
-  let ingredients = [];
-  let steps = recipeLines;
-  while (recipeLines.length > 0) {
-    // Each ingredient line starts with a number, once we hit the first non number we've reached the steps and we can
-    // stop pulling the lines into the ingredients array.
-    if (!isNaN(parseInt(recipeLines[ 0 ][ 0 ], 10))) {
-      ingredients.push(recipeLines.shift());
-    }
-    else {
-      break;
-    }
-  }
-
-  return { name, ingredients, steps };
+let files = [ 'appetizers' ];
+let textBlobs = { };
+files.forEach(file => {
+  textBlobs[ file ] = fs.readFileSync(`./${file}.txt`, "utf8");
 });
 
-recipes.forEach(recipe => {
-  recipe.ingredients = recipe.ingredients.map(ingredientString => {
-    // expecting ingredients to be strings of the form '2/3 cups unbleached baking flour'
-    let parts = ingredientString.split(' ');
+Object.keys(textBlobs).forEach(file => {
+  let textBlob = textBlobs[ file ];
+  let recipes = textBlob.split('\n\n');       // blank lines denote recipe blobs
+  recipes = recipes.map(r => r.split('\n'));  // turn each blob into an array of lines
+  recipes = recipes.map(recipeLines => {
+    let name = recipeLines.shift();
+    let ingredients = [];
+    let steps = recipeLines;
+    while (recipeLines.length > 0) {
+      // Each ingredient line starts with a number, once we hit the first non number we've reached the steps and we can
+      // stop pulling the lines into the ingredients array.
+      if (!isNaN(parseInt(recipeLines[ 0 ][ 0 ], 10))) {
+        ingredients.push(recipeLines.shift());
+      }
+      else {
+        break;
+      }
+    }
 
-    let quantity = parts.shift();
-    let unit = parts.shift().split('_').join(' ');
-    let ingredient = parts.join(' ');
+    return { name, ingredients, steps };
+  });
 
-    return { quantity, unit, ingredient };
-  })
+  recipes.forEach(recipe => {
+    recipe.ingredients = recipe.ingredients.map(ingredientString => {
+      // expecting ingredients to be strings of the form '2/3 cups unbleached baking flour'
+      let parts = ingredientString.split(' ');
+
+      let quantity = parts.shift();
+      let unit = parts.shift().split('_').join(' ');
+      let ingredient = parts.join(' ');
+
+      return { quantity, unit, ingredient };
+    })
+  });
+
+  console.log(recipes[0]);
+
 });
-
-console.log(recipes[1]);
 
 //recipes = recipes.map(s => ({
 //  section: s[0],
